b66259bc60de23e2e115d8e8ec596e47e48cc089
TheN00b21 <73141568+TheN00b21@users.noreply.github.com>
2021-09-26 11:42:34 -0700

    Fixed minor spelling errors and cleaned up code

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main() {
    int enx;
    int ftype;
    char namefile[100];
-   //Unsed Content//
+   //Unused Content//
    int cfile = 1;
    int mfile = 2;
    int bin = 3;
@@ -43,7 +43,7 @@ int main() {
             sleep(100);
          }
       } else if (ftype == 3) {
-         printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\Hi you found my secret! Either that or stop being a stalker and looking at this messy source code.");
+         printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nHi you found my secret! Either that or stop being a stalker and looking at this messy source code.");
          sleep(100);
       } else {
          printf("You did not enter 1 or 2. Please exit and try again");
@@ -84,7 +84,7 @@ int main() {
 
    } else if (enx == 3) {
 
-      printf("\n\n\n     Thanks so much for downloading my tool! I really hope this is a program will help you. Made by TheN00b21.\n\n\t\t\t\t\t\t\t   FAQ\n\n");
+      printf("\n\n\n     Thanks so much for downloading my tool! I really hope this program will help you. Made by TheN00b21.\n\n\t\t\t\t\t\t\t   FAQ\n\n");
       printf("Why is my file not extracting/compressing?\nThis may be multiple problems, 1. Your file is not named the original name it was in the game or when it was extracted, 2. Your file is not in the directory of this program, 3. Wiimm's szs tools are not installed.\n\n");
       printf("How do I control this program?\nWell you probably already figured it out by now but you press 1, 2, or 3 on your keyboard and press enter to navigate.\n\n");
       printf("Why did you create this program?\nGood Question! There are many reasons but the main reason is to have a more lightweight and accessible version of converting MSM files.\n\n");
